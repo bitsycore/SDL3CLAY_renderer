35bdcefd7a517567306154e530c79b2ca928d080
bitsy <bitsydoge@proton.me>
2025-02-15 23:13:12 +0100

    FIX: some Arena change to use uintptr instead

--- src/common/arena.h
+++ src/common/arena.h
@@ -2,14 +2,13 @@
 #define ARENA2_H
 
 #include <stdlib.h>
-#include <stddef.h>
 
 #include "../common/error_handling.h"
 
 typedef struct {
 	size_t capacity;
-	void* current;
-	void* buf;
+	uintptr_t current;
+	uintptr_t buf;
 } Arena;
 
 #if !( defined(__STDC_VERSION__) && __STDC_VERSION__ >= 201112L )
@@ -18,33 +17,33 @@ typedef union {
 	long double ld;
 	void* p;
 } max_align_t;
+#else
+#include <stddef.h>
 #endif
 
 #define Arena_requiredSize(size) (sizeof(Arena) + size)
-
 #define Arena_init(buffer, size) ({ \
-	Arena* arena = (Arena*)(buffer); \
+	Arena* arena = (buffer); \
 	EXIT_IF(\
 		(size) <= sizeof(Arena),\
 		"Capacity must be greater than the size of Arena, "\
 		"use Arena_requiredSize to calculate the required size"\
 	);\
 	arena->capacity = (size) - sizeof(Arena); \
-	arena->current = (char*)arena + sizeof(Arena); \
+	arena->current = (uintptr_t)arena + sizeof(Arena); \
 	arena->buf = arena->current; \
-	arena; \
+	(void*)arena; \
 })
 
 #define Arena_alloc(arena, size) ({ \
 	EXIT_IF(arena == NULL, "Arena is NULL"); \
-	EXIT_IF(size <= 0, "Size must be greater than 0"); /*NOLINT(*-sizeof-expression)*/ \
-	void* aligned_current = (void*)((uintptr_t)arena->current + (sizeof(max_align_t) - 1) & ~(sizeof(max_align_t) - 1));\
+	EXIT_IF(size == 0, "Size must be greater than 0"); \
+	const uintptr_t aligned_current = arena->current + (sizeof(max_align_t) - 1) & ~(sizeof(max_align_t) - 1);\
 	arena->buf + arena->capacity - aligned_current < size\
 		? NULL\
 		: ({\
-			void* ptr = aligned_current;\
-			arena->current = (char*) aligned_current + size;\
-			ptr;\
+			arena->current = aligned_current + size;\
+			(void*)aligned_current;\
 		});\
 })
 
